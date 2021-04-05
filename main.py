21cf69b90156ddac7667a5e08ad253be24eb6ead
owenmather <owenmather7@gmail.com>
2021-04-05 16:59:59 +0200

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+base_url = ""
+
+def get_top_news_ids():
+    return None
+
+def get_comments(post):
+    return None
+
+if __name__ == "__main__":
+    top = get_top_news_ids()
+
+    for post in top:
+        comments = get_comments(post)
